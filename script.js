433f38fabc81dbfdc917a22e7571d9dd5fc76cc8
MDTufan <129486543+MDTufan@users.noreply.github.com>
2023-06-27 18:52:05 -0700

    Add files via upload
    documation

---
+++ script.js
@@ -0,0 +1,158 @@
+//  JavaScript Output..........  
+
+// alert("Hello");
+// console.log("Tufan ali");
+// document.write("whats you name?")
+
+// document.getElementById("one").innerHTML = "My Name Tufan ali.";
+
+
+// function chengecontent() {
+//     document.getElementById("chenge1").innerHTML="My name is Toli."
+// }
+  
+
+
+
+
+
+
+
+
+//   js statements........
+
+// var Name1 = "Tufan Ali";
+// var a = 5;
+// var b = 8;
+// var c = a + b;
+// console.log(c);
+
+// let fastName = "Tufan ";
+// let lastName = "Ali";
+// let FullName = fastName + lastName;
+// console.log(FullName);
+
+// let num1 = 30;
+// let num2 = 10;
+
+// let num3 = num1 + num2;
+// console.log(num3);
+
+// let num4 = num1 * num2;
+// console.log(num4);
+
+// const RollNumber = 512;
+// console.log(RollNumber);
+
+
+// Comments..........
+// your fast name Toli.
+/* your fast name toli*/
+
+
+// JavaScript Variables.........
+
+
+// var num1 = 7;
+// let FatherName = "Ibrahim Hosson";
+// const Roll = 102;
+// console.log(num1);
+// console.log(FatherName);
+// console.log(Roll);
+
+
+// JavaScript Operators..........
+// Arithmetic Operators
+// +,-,*,/,**,%,++,--,
+
+// let num1 = 30;
+// let num2 = 10
+// let num3 = num1 + num2;
+// console.log(num3);
+
+// let num4 = num1 * num2;
+// console.log(num4);
+
+// let num5 = num1/num2;
+// console.log(num5);
+
+// let num6 = num1 - num2;
+// console.log(num6);
+
+// let num7 = num1 % num2;
+// console.log(num7);
+
+
+
+
+// Assignment Operators
+// =,+=,-=,*=,/=,%=,**=,
+
+// let number = 70;
+
+// let number1 = 20;
+//   number1 += 10;
+//   console.log(number1);
+
+// let number2 = 20;
+//   number2 -= 10;
+//   console.log(number2);
+
+// let number3 = 20;
+//   number3 *= 10;
+//   console.log(number3);
+
+// let number4 = 20;
+//   number4 /= 10;
+//   console.log(number4);
+
+// let number5 = 20;
+//   number5 %= 10;
+//   console.log(number5);
+
+// let number6 = 3;
+//   number6 **= 3;
+//   console.log(number6);
+
+
+
+//Comparison Operators
+// ==,===,!=,!==,<,>,!,?
+ 
+// var num1 = 20;
+// var num2 = 10;
+// var num3 = "10";
+// var num4 = 20;
+// var num5 = 15;
+
+// console.log(num1 > num2); 
+// console.log(num1 >= num2);
+// console.log(num1 < num2);
+// console.log(num1 <= num2);
+// console.log(num1 == num4);
+// console.log(num1 != num4);
+// console.log(num1 === num3);
+// console.log("num2 === num3 : " + (num2 === num3));
+// console.log("num2 == num3 : " + (num2 == num3));
+// console.log(num1 !== num3);
+
+//Logical Operators
+// &&,||,!
+
+// logical operators helps us to compine multiple conditions
+// console.log(num1 > num2 && num1 > num5);
+// console.log(num1 > num2 || num1 > num5);
+
+
+// JavaScript Functions.........
+
+
+// function fastnumber( num1, num2) {
+//      let A = num1;
+//      let B = num2;
+//      let C = num1 * num2;
+//      return C ;
+// }
+
+// let result = fastnumber(12,6);
+// console.log(result);
